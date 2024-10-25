56163ed51c3ed8988a5bb60ec6d6921708efea0e
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2024-10-26 00:34:38 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 127 ms, Memory: 44,796 KB -BaekjoonHub

--- SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
@@ -1,17 +1,31 @@
+
 for test_case in range(1, 11):
     n = int(input())
     arr = input().split()
     m = int(input())
-    o = input().split('I ')
-    for i in range(1,m+1):
-        a = o[i].split(' ')
-        b = int(a[0])
-        for j in range(2,int(a[1])+2):
-            arr.insert(b,a[j])
-            b+=1
+    o = input().split()
+    for i in range(len(o)):
+        if(o[i]=='I'):
+            a = int(o[i+1])
+            b = int(o[i+2])
+            c = i+3
+            for j in range(c, c+b):
+                arr.insert(a,o[j])
+                a+=1
+
+        if(o[i]=='D'):
+            a = int(o[i+1])
+            b = int(o[i+2])
+            for j in range(b):
+                arr.pop(a)
+    # for i in range(1,m+1):
+    #     a = o[i].split(' ')
+    #     b = int(a[0])
+    #     for j in range(2,int(a[1])+2):
+    #         arr.insert(b,a[j])
+    #         b+=1
 
     print(f'#{test_case} ',end='')
     for i in range(10):
         print(arr[i],end=' ')
     print()
-
